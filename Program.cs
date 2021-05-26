d60b6e438758adb1a2e4261c9da4e551d2b24950
georgecolli <collingridgegeorge@gmail.com>
2021-05-26 23:57:49 +0100

    added player1 option to add name

--- Program.cs
+++ Program.cs
@@ -8,8 +8,13 @@ namespace WarriorBattle
         static void Main(string[] args)
         {
             Clear();
+
             Console.WriteLine("Player 1, what is your name?");
             string player1Name = ReadLine().Trim();
+
+
+
+
             Warrior hercules = new Warrior("Hercules", 1000);
             Warrior ben = new Warrior("Ben", 1000);
             Console.WriteLine(hercules.Speak(5)); //low enough that it will return a particular phrase
