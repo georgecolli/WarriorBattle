f31b906b4f3fccbf22b163a509e67f435fd6e021
georgecolli <collingridgegeorge@gmail.com>
2021-05-27 00:00:08 +0100

    added player 2 option to add name

--- Program.cs
+++ Program.cs
@@ -12,16 +12,16 @@ namespace WarriorBattle
             Console.WriteLine("Player 1, what is your name?");
             string player1Name = ReadLine().Trim();
 
+            Console.WriteLine("Player 2, what is your name?");
+            string player2Name = ReadLine().Trim();
 
-
-
-            Warrior hercules = new Warrior("Hercules", 1000);
-            Warrior ben = new Warrior("Ben", 1000);
-            Console.WriteLine(hercules.Speak(5)); //low enough that it will return a particular phrase
-            Console.WriteLine(ben.Speak(1)); //low enough that it will return a particular phrase
-            Console.WriteLine(hercules.Speak(10)); //high enough to return a random phrase
-            Console.WriteLine(ben.Speak(8)); //high enough to return a random phrase
-            Battle.Play(ben, hercules);
+            Warrior player1 = new Warrior(player1Name, 1000);
+            Warrior player2 = new Warrior(player2Name, 1000);
+            Console.WriteLine(player1.Speak(5)); //low enough that it will return a particular phrase
+            Console.WriteLine(player2.Speak(1)); //low enough that it will return a particular phrase
+            Console.WriteLine(player1.Speak(10)); //high enough to return a random phrase
+            Console.WriteLine(player2.Speak(8)); //high enough to return a random phrase
+            Battle.Play(player2, player1);
         }
     }
 }
